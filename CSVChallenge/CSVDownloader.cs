6cba5aa5ef2133326aacdb3fc797b0d3bfb24cc4
kyleoettle <oettle.k@gmail.com>
2021-02-03 15:59:14 +0200

    initial commit

---
+++ CSVChallenge/CSVDownloader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVChallenge
+{
+    public class CSVDownloader
+    {
+
+        public static async Task<IEnumerable<string>> DownlaodAsync(string url)
+        {
+            string result;
+            using (var client = new HttpClient())
+            {
+
+                var response = await client.GetAsync(url).ConfigureAwait(false);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return result.Split('\n');
+                }
+            }
+            return null;
+
+        }
+    }
+}
